6ec75c6216022c0e17df46538f7b5a055606d93f
jorgebran2002 <jorgebran2002@gmail.com>
2020-01-12 13:28:01 -0600

    12-1-2020
    Creación de servicio

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService 
+{
+
+  listas: Lista[] = [];
+
+  constructor() 
+  {
+    console.log('->Servicio Inicializado');
+  }
+
+}
